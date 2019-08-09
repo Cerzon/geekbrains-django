59faca66b0d76fe0d6034facaab6d8ce1d1941a1
Cerzon <kirill.tkachenko@gmail.com>
2019-08-09 03:48:37 +0300

    django lesson2 homework complete

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,38 @@
 from django.shortcuts import render
+import json
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    text_data = """С точки зрения банальной эрудиции
+        не каждый локально мыслящий индивидуум
+        способен утрировать теории физикоабстракции.
+
+        Из жевательной резинки, килограммов из пяти, можно вылепить ботинки для последнего пути."""
+    context_dict = {
+        'page_title': 'Главная',
+        'img_src': 'mainapp/img/main.jpg',
+        'text_data': text_data,
+    }
+    return render(request, 'mainapp/index.html', context_dict)
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    context_dict = {
+        'page_title': 'Продукты',
+        'img_src': 'mainapp/img/products.jpg',
+        'text_data': [
+            'если б мишки были пчёлами,',
+            'то они бы нипочём',
+            'ни за что и не подумали',
+            'так высоко строить дом.',
+            'и тогда, конечно если бы',
+            'пчёлы это были б мишки,',
+            'нам бы, мишкам, не пришлось бы',
+            'лазить на такие вышки.',
+        ],
+    }
+    return render(request, 'mainapp/products.html', context_dict)
 
 def contacts(request):
-    return render(request, 'mainapp/contacts.html')
+    with open('media/data/contacts.json', 'r', encoding='utf-8') as data_file:
+        context_dict = json.load(data_file)
+    print(type(context_dict['addresses']))
+    return render(request, 'mainapp/contacts.html', context_dict)
