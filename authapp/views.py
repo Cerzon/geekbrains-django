e18e11a86005c6cd8b9bac8f50e133c44ff0e608
Cerzon <kirill.tkachenko@gmail.com>
2019-08-19 17:44:40 +0300

    register/edit urls added

--- authapp/views.py
+++ authapp/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
+from django.contrib import auth
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy, reverse
+from .forms import ShopUserLoginForm
 
 def login(request):
-    return render(request, 'authapp/login.html')
+    login_form = ShopUserLoginForm(data=request.POST)
+    if request.method == 'POST' and login_form.is_valid():
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user and user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+    context_dict = {
+        'page_title': 'вход',
+        'form': login_form,
+    }
+    return render(request, 'authapp/login.html', context_dict)
 
 
 def logout(request):
+    auth.logout(request)
     return HttpResponseRedirect(reverse('index'))
