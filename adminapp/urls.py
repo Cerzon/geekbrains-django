1c96566971b0ed055fd4d6dd2c48d872ce90c5c4
Cerzon <kirill.tkachenko@gmail.com>
2019-08-28 18:30:05 +0300

    просмотр товаров с фильтрацией по категориям

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -4,5 +4,10 @@ import adminapp.views as adminapp
 app_name = 'adminapp'
 
 urlpatterns = [
-    path('',,),
+    path('products/list/', adminapp.ProductListView.as_view(), name='product_list'),
+    path('products/list/category/<int:category_id>/', adminapp.ProductListView.as_view(), name='cat_product_list'),
+    path('products/detail/<int:pk>/', adminapp.ProductDetailView.as_view(), name='product_detail'),
+    path('products/create/', adminapp.CreateProductView.as_view(), name='product_create'),
+    path('products/update/<int:pk>/', adminapp.UpdateProductView.as_view(), name='product_update'),
+    path('products/delete/<int:pk>/', adminapp.DeleteProductView.as_view(), name='product_delete'),
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView
+from django.views.generic.list import ListView
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse, reverse_lazy
+from mainapp.models import Product, ProductCategory
 
-# Create your views here.
+
+class CreateProductView(CreateView):
+    pass
+
+
+class UpdateProductView(UpdateView):
+    pass
+
+
+class DeleteProductView(DeleteView):
+    pass
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'adminapp/product_list.html'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.kwargs.get('category_id', False):
+            queryset = queryset.filter(category__pk=self.kwargs['category_id'])
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        categories = ProductCategory.objects.all()
+        context['title'] = 'Товары'
+        if self.kwargs.get('category_id', False):
+            context['title'] += ' в категории "' + categories.get(pk=self.kwargs['category_id']).name + '"'
+        context['categories'] = ProductCategory.objects.all()
+        return context
+    
+
+
+class ProductDetailView(DetailView):
+    pass
